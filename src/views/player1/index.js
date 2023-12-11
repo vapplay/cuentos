25730cfaa473435ff97f7befe06d54ea666c2df9
Vapplay <vapplay.dev@gmail.com>
2023-12-11 18:42:16 -0500

    Boton_Back

--- src/views/player1/index.js
+++ src/views/player1/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {View, Text, Button, TouchableOpacity} from 'react-native';
+import {View, Text, Button, TouchableOpacity, TextInput} from 'react-native';
 import {stylesP1} from './stylesP1'; // Asegúrate de importar tus estilos correctamente
+
 import {AppRegistry} from 'react-native';
 import App from '../../../App';
 import {name as appName} from '../../../app.json';
 import {playbackService} from './trackPlayerServices';
 import {useEffect, useState} from 'react';
+import { Share } from 'react-native-share';
 import {
   SafeAreaView,
   StyleSheet,
@@ -23,11 +25,84 @@ import TrackPlayer, {
 //import { Icon } from 'react-native-elements';
 import {Icon} from '@rneui/themed';
 import {Image} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { onShare } from './App2';
+
 
 AppRegistry.registerComponent(appName, () => App);
 TrackPlayer.registerPlaybackService(() => playbackService);
 
+
+
+
+// const handleShare = () => {
+//   // Comparte el texto "¡Hola, mundo!"
+//   Share.share(
+//     {
+//       title: '¡Hola, mundo!',
+//       message: '¡Hola, mundo!',
+//     },
+//     {
+//       dialogTitle: 'Compartir con...',
+//       subject: 'Un saludo',
+//     }
+//   )
+//     .then((result) => {
+//       if (result.action === Share.sharedAction) {
+//         alert('Contenido compartido');
+//       } else if (result.action === Share.dismissedAction) {
+//         alert('Compartir cancelado');
+//       }
+//     })
+//     .catch((error) => {
+//       alert('Ocurrió un error: ' + error.message);
+//     });
+// };
+
+
+
+
+const App2 = () => {
+  const [appId, setAppId] = useState('com.example.myapp');
+
+  const onShare = () => {
+    
+    try {
+      Share.share({
+        title: '¡Descarga mi app!',
+        message: 'Esta app es genial, ¡descárgala ahora!',
+        url: `https://play.google.com/store/apps/details?id=${appId}`,
+      });
+    } catch (error) {
+      alert('Ocurrió un error al compartir: ' + error.message);
+    }
+  };
+
+  
+  return (
+    <View>
+      <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        value={appId}
+        onChangeText={(text) => setAppId(text)}
+      />
+      <Button title="Compartir" onPress={onShare} />
+    </View>
+  );
+};
+
+//export default App2;
+
+
+
+
+
+
+
+
+
 function Controls({onShuffle}) {
+  const navigation = useNavigation();
   const playerState = usePlaybackState();
 
   const [play, setPlay] = useState(Boolean);
@@ -42,10 +117,18 @@ function Controls({onShuffle}) {
     }
   }
 
+
+
+
+
+
+
+
+  
   return (
     <View style={stylesP1.controlsContainer}>
 
-<TouchableOpacity style={stylesP1.buttonShare} onPress={handlePlayPress}>
+<TouchableOpacity style={stylesP1.buttonShare} onPress={() => App2.onShare()}>
         <Text style={stylesP1.buttonText}>Compartir</Text>
       </TouchableOpacity>
 
@@ -53,9 +136,11 @@ function Controls({onShuffle}) {
       <TouchableOpacity style={stylesP1.buttonPlay} onPress={handlePlayPress}>
         <Text style={stylesP1.buttonText}>{play ? 'Play' : 'Pause'}</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={stylesP1.buttonBack} onPress={() => navigation.navigate('home')} >
+      <TouchableOpacity style={stylesP1.buttonBack} onPress={() => navigation.goBack()} >
         <Text style={stylesP1.buttonText}>atras</Text>
       </TouchableOpacity>
+
+
       <Icon name="rowing" />
 
       <Icon name="sc-telegram" type="evilicon" color="#517fa4" />
@@ -91,7 +176,7 @@ function PlayerScreen({ navigation}) {
 
   return (
     <SafeAreaView style={stylesP1.container}>
-      <Text style={stylesP1.title}>Marie Curie, una Niña Científica 1234 ¡</Text>
+      <Text style={stylesP1.title}>Marie Curie, una Niña Científica 123 ¡</Text>
 
       <Image
         source={require('./images/marieCurie1.jpg')} // Reemplaza 'ruta/de/tu/imagen.png' con la ruta de tu imagen
@@ -111,12 +196,43 @@ function PlayerScreen({ navigation}) {
     </SafeAreaView>
   );
 
+
+
+
+
+
+
+
+  
   // return (
   //   <SafeAreaView style={stylesP1.container}>
   //     <Button title="Play" color="#777" onPress={() => TrackPlayer.play()}/>
   //   </SafeAreaView>
   // );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // return (
 //   <View style={styles.containerPlayer}>
